506653cc1742cfd0cb2d8a2631871b0605d20eb1
nitiponth <67319636+nitiponth@users.noreply.github.com>
2020-08-05 13:38:20 +0700

    Update Lab4P1Qt.ino

--- Arduino Hex Code/Lab4P1Qt/Lab4P1Qt.ino
+++ Arduino Hex Code/Lab4P1Qt/Lab4P1Qt.ino
@@ -1,6 +1,5 @@
 char singlePhase[4] = {0x01,0x02,0x04,0x08};
 char fullPhase[4] = {0x03,0x06,0x0C,0x18};
-//char fullDoublePhase[8] = {0x03,0x03,0x06,0x06,0x0C,0x0C,0x18,0x18};
 char halfPhase[8] = {0x01,0x03,0x02,0x06,0x04,0x0C,0x08,0x18};
 int qt = 0;
 
@@ -10,10 +9,6 @@ void setup() {
   DDRB = 0x0F;    //0b0000 1111
   DDRD = 0xE3;    //0b1110 0011
   PORTD = PORTD | 0b00011100;
-
-  //PORTB = singlePhase[0];
-  //PORTB = fullPhase[0];
-  //PORTB = halfPhase[0];
 }
 
 void loop() {
@@ -35,8 +30,7 @@ void loop() {
     while(qt != 0)
       rotate();
   }
-  PORTB = 0x00;
-  
+  PORTB = 0x00; 
 }
 
 void rotate(){
