58874ce15a5204c75d1753de1236c71d846b0841
nitiponth <ntitiponth@outlook.com>
2020-09-26 16:08:47 +0700

    load temp to dB

---
+++ MiniProject/tempToDB/tempToDB.ino
@@ -0,0 +1,88 @@
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#include <ctime>
+#include <iostream>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define FIREBASE_HOST "esp32test-9f958.firebaseio.com"
+#define FIREBASE_AUTH "DzD4zyUm7S8nk6E64zcsjpSObEPWYVadsG0Imw5L"
+#define WIFI_SSID "Room221_2.4G"
+#define WIFI_PASSWORD "Jummaidai"
+FirebaseData firebaseData;
+const int oneWireBus = 4;
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
+
+
+void setup(){
+  
+  Initialization();
+  WiFiConnection();
+  sensors.begin();
+ }
+
+void Initialization(){
+  
+  Serial.begin(115200); 
+ }
+
+void WiFiConnection(){
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+}
+
+
+int logCount = -1;
+int counter = 1;
+float tempC = 0.0;
+
+void loop(){
+  if (Firebase.setTimestamp(firebaseData,"/serverTime")){
+      std::time_t result = firebaseData.intData() + 25200;
+      String date = asctime(std::localtime(&result));
+      date.trim();
+      String year = date.substring(20,24);
+      String month = "/" + date.substring(4,7);
+      String day = "/" + date.substring(8,10);
+      String hour = "/" + date.substring(11,13);
+      String min = "/" + date.substring(14,16);
+      int minInt = min.substring(1,4).toInt();
+      
+      //sensors.requestTemperatures();
+      //tempC = sensors.getTempCByIndex(0);
+      //Serial.println(tempC);
+      
+      if(minInt%5 == 0){
+        if (Firebase.setString(firebaseData,year + month + day + hour + min + "/date",date)){
+          Serial.println("SUCCESS to add DATE to database : \n" + date);
+        }
+        else{
+          Serial.println("FAIL ==> REASON: " + firebaseData.errorReason());
+        }
+        sensors.requestTemperatures();
+        tempC = sensors.getTempCByIndex(0);
+        if(Firebase.setFloat(firebaseData,year + month + day + hour + min + "/temperature",tempC)){
+          Serial.println("SUCCESS to add TEMP to database : ");
+          Serial.println(tempC);
+        }
+        else{
+          Serial.println("FAIL ==> REASON: " + firebaseData.errorReason());
+        }
+      }
+  }
+
+  delay(10000);
+}
